0bef733d8b9a98d0bebfb62a834f356cefe40b62
Anthony Daniel <anthonynobom100@gmail.com>
2023-07-14 21:24:02 -0300

    Adição da API

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,37 @@
+import {useNavigate} from 'react-router-dom'
 import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
 
 function NewProject() {
+
+    const history = useNavigate()
+
+    function createPost(project){
+        
+        //initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch("http:/localhost:5000/project", {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp)=> resp.json())
+        .then((data) => {
+                console.log(data)
+            })
+        .catch(err => console.log(err))
+
+    }
+
     return (
         <div className={styles.npContainer}>
             <h1>Create Project</h1>
             <p>Create your Project and then add the services</p>
-            <ProjectForm btnText="Create Project"/>
+            <ProjectForm handleSubmit={createPost} btnText="Create Project"/>
         </div>
     )
 }
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,28 @@
+import { useState, useEffect} from "react"
+
 import Select from "../form/Select"
 import Submit from "../form/Submit"
 import Input from "../form/input"
 import styles from "./ProjectForm.module.css"
 
-function ProjectForm({btnText}){
+function ProjectForm({handleSubmit, btnText, projectData}){
+
+    const [categories, setCategories] = useState([]) 
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'Content-type': 'application/json',
+        },
+    })
+        .then((resp) => resp.json())
+        .then((data)=>{
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input 
@@ -17,9 +36,13 @@ function ProjectForm({btnText}){
             text="Budget of project"
             name="budget"
             placeholder="Enter the budget"
-            />
+            /> 
             
-            <Select name="categoy_id" text="Select the category"/>
+            <Select 
+            name="categoy_id" 
+            text="Select the category" 
+            options={categories}
+            />
 
             <Submit text={btnText}/>
         </form>
